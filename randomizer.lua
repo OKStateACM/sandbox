24acccd0965800d6953749476ef1b0c1764fd78f
Alex <o0o.moro.o0o@gmail.com>
2016-09-21 17:04:04 -0500

    added a simple lua program to generate a random list

---
+++ randomizer.lua
@@ -0,0 +1,25 @@
+--first, we read in the file
+local filename = 'names.txt'
+local file = io.open(filename)
+
+--first we will read in all the names into one big list
+local names = {}
+while true do
+	local line = file:read("*l") --read in a line
+	if not line then break end --stop when we reach the end
+	table.insert(names, line) --add this name to our list
+end
+
+--Now we select some random names and add it to a smaller list
+local number, outputList = 10, {} --define how many tames we want to output, and an empty list for our output
+for i=1,number do --simple for loop construction. iterates from 1 to number, so it will repeat *number* times.
+	local position = math.random(1,#names) --we select a random line number
+	local name = table.remove(names, position)
+	table.insert(outputList, name) --and add it to the output list
+end
+
+
+--and finally, print out the final lists.
+for index, name in ipairs(outputList) do
+	print(name)
+end
---
+++ tasks.md
@@ -0,0 +1,4 @@
+* change the number of names
+* only output names that start with a certain letter
+	* make the choice of letter random
+* 
